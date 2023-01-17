7443f6dd4b7fa3cbe7e92dbf765fdb591bdd06a0
thomashummler <t.hummler@hummler-gmbh.de>
2023-01-17 17:11:32 +0100

    vue

--- assets/js/promote.js
+++ assets/js/promote.js
@@ -1,17 +1,55 @@
 parasails.registerPage("promote", {
     data: {
-        Promotion: 0
+        Promotion: 0,
+        eventId : 0
+
     },
     methods: {
-        find: function () {
-            let origin = window.location.origin
-            let url = new URL(origin + '/eventall')
+
+        promote: function () {
+            const formData = {
+                promotionStatus : this.Promotion
+            }
+            let origin = window.location.origin;
+            let url = new URL(origin + '/api/v1/event/promotionUpdate');
+            const body = JSON.stringify(formData);
+            console.log(body);
+            const postForm =(body) => {
+                return fetch('/api/v1/reservation/create', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body
+                });
+            }
+            postForm(body)
+                .then(res => res.json())
+                .then(data => {
+                    
+                    let kk = data.promotionStatus;
+                    console.log("kk")
+                })
+        }
+        
+
+
+        
+        /*find: function () {
+            let origin = window.location.origin;
+            let url = new URL(origin + '/eventall');
             fetch(url)
                 .then(res => res.json())
                 .then(data => {
-                    console.log(data);
+                    this.events = data;
+                    data.forEach((res) => {s
+                        console.log(res)
+                    })
                 })
+        },*/
 
-        },
+
+
+
     }
 })
